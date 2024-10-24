3b4f8409bffb6c1e41fba5461798e1721ba201aa
Hany Sabeh <9498740+Hanysabeh@users.noreply.github.com>
2024-10-24 12:55:40 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,35 +1,59 @@
 #!/bin/bash
 
 # Define variables
-PACKAGE_URL="http://example.com/path/to/your/application/package.tar.gz"
-INSTALL_DIR="/opt/your_application"
+REPO_URL="https://api.github.com/repos/Hanysabeh/YAS-pedistrain-camera/releases/latest"
+DOWNLOAD_DIR="/opt/YAS-pedistrain-camera"
+SERVICE_NAME="YAS-pedistrain-camera"
 
-# Create install directory if it doesn't exist
-mkdir -p $INSTALL_DIR
+# Create download directory if it doesn't exist
+mkdir -p $DOWNLOAD_DIR
 
-# Download the package
-wget -O $INSTALL_DIR/package.tar.gz $PACKAGE_URL
+# Download the latest release archive
+curl -s $REPO_URL | jq -r '.assets[] | select(.name | contains("tar.gz")) | .browser_download_url' | xargs wget -P $DOWNLOAD_DIR
 
 # Extract the package
-tar -xzvf $INSTALL_DIR/package.tar.gz -C $INSTALL_DIR
+tar -xzvf $DOWNLOAD_DIR/package.tar.gz -C $DOWNLOAD_DIR
 
 # Make the main executable script executable
-chmod +x $INSTALL_DIR/your_application_executable
+chmod +x $DOWNLOAD_DIR/YAS-pedistrain-camera/install.sh
 
 # Create a desktop shortcut (optional)
 cat <<EOF > ~/.local/share/applications/your_application.desktop
 [Desktop Entry]
 Name=Your Application
-Exec=$INSTALL_DIR/your_application_executable
-Icon=$INSTALL_DIR/icon.png
+Exec=$DOWNLOAD_DIR/YAS-pedistrain-camera/install.sh
+Icon=$DOWNLOAD_DIR/icon.png
 Terminal=false
 Type=Application
 EOF
 
-echo "Installation complete!"
+# Create a systemd service
+cat <<EOF > /etc/systemd/system/$SERVICE_NAME.service
+[Unit]
+Description=YAS-pedistrain-camera
+After=network.target
 
-# chmod +x ped.sh
-# sudo ./ped.sh
+[Service]
+ExecStart=$DOWNLOAD_DIR/your_application_executable
+WorkingDirectory=$DOWNLOAD_DIR
+Restart=always
+User=$(whoami)
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Reload systemd manager configuration
+systemctl daemon-reload
+
+# Enable the service to start on boot
+systemctl enable $SERVICE_NAME
+
+# Start the service
+systemctl start $SERVICE_NAME
+
+echo "Installation and service setup complete!"
+
 
 
 # tar -czf my_app.tar.gz /path/to/your/app/files /path/to/your/config.json install.sh
